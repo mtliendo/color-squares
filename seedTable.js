8b3da8dd5643a9e4edc5b732461b5e9f29f80cf6
focusotter <mtliendo@focusotter.com>
2024-07-08 15:41:15 -0500

    needs styling but working

---
+++ seedTable.js
@@ -0,0 +1,58 @@
+import { DynamoDBClient, BatchWriteItemCommand } from '@aws-sdk/client-dynamodb'
+import { fromIni } from '@aws-sdk/credential-providers'
+import { marshall } from '@aws-sdk/util-dynamodb'
+import { v4 as uuidv4 } from 'uuid'
+const credentialProvider = fromIni()
+
+const dynamodb = new DynamoDBClient({ credentialProvider })
+
+async function seedDynamoDBTable(tableName, itemCount) {
+	const params = {
+		RequestItems: {
+			[tableName]: [],
+		},
+	}
+
+	for (let i = 0; i < itemCount; i++) {
+		const item = marshall({
+			// Define your item properties here
+			id: uuidv4(),
+			__typename: 'Cell',
+			color: `#ddd`,
+			index: i,
+			createdAt: new Date().toISOString(),
+			updatedAt: new Date().toISOString(),
+		})
+
+		params.RequestItems[tableName].push({
+			PutRequest: {
+				Item: item,
+			},
+		})
+
+		if (params.RequestItems[tableName].length === 25) {
+			try {
+				const batchWriteCommand = new BatchWriteItemCommand(params)
+				await dynamodb.send(batchWriteCommand)
+				params.RequestItems[tableName] = []
+				console.log(`Inserted ${i + 1} items.`)
+			} catch (err) {
+				console.error('Error inserting items:', err)
+				break
+			}
+		}
+	}
+
+	if (params.RequestItems[tableName].length > 0) {
+		try {
+			const batchWriteCommand = new BatchWriteItemCommand(params)
+			await dynamodb.send(batchWriteCommand)
+			console.log(`Inserted ${itemCount} items.`)
+		} catch (err) {
+			console.error('Error inserting items:', err)
+		}
+	}
+}
+
+// Usage example
+seedDynamoDBTable('Cell-fcvtjhylrjfzvasg7adk2i3hyq-NONE', 2000)
---
+++ src/App.tsx
@@ -0,0 +1,136 @@
+import { Amplify } from 'aws-amplify'
+import { useEffect, useState } from 'react'
+import awsconfig from '../amplify_outputs.json'
+import { generateClient } from 'aws-amplify/api'
+import { Schema } from '../amplify/data/resource'
+Amplify.configure(awsconfig)
+
+const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'white', 'black']
+const client = generateClient<Schema>()
+
+const Cell = ({
+	item,
+	selectedColor,
+}: {
+	item: Schema['Cell']['type']
+	selectedColor: string
+}) => {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+	return (
+		<div
+			key={item.id}
+			className="w-6 h-6 border border-black"
+			style={{ backgroundColor: item.color }}
+			onClick={async () => {
+				console.log('you clicked', item.id)
+				await client.models.Cell.update({
+					id: item.id,
+					color: selectedColor,
+				})
+			}}
+		/>
+	)
+}
+const Grid = ({
+	selectedColor,
+	items,
+}: {
+	selectedColor: string
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	items: any[]
+}) => {
+	console.log('incoming data', items)
+	return (
+		<div className=" flex flex-wrap max-w-[1500px]">
+			{items.map((item) => {
+				return <Cell key={item.id} item={item} selectedColor={selectedColor} />
+			})}
+		</div>
+	)
+}
+
+function App() {
+	const [selectedColor, setSelectedColor] = useState('')
+	const [items, setItems] = useState<
+		{
+			id: string
+			index: number
+			color: string
+		}[]
+	>([])
+
+	useEffect(() => {
+		client.models.Cell.list({ limit: 2000 })
+			.then((response) => {
+				console.log('triggered')
+				setItems(response.data)
+			})
+			.catch((error) => {
+				console.error(error)
+			})
+	}, [])
+
+	useEffect(() => {
+		const sub = client.models.Cell.onUpdate().subscribe({
+			next: (data) => {
+				console.log('update', data)
+				console.log(data.id)
+				setItems((prevItems) =>
+					prevItems.map((item) =>
+						item.id === data.id ? { ...item, color: data.color } : item
+					)
+				)
+			},
+			error: (error) => {
+				console.error(error)
+			},
+		})
+
+		return () => {
+			sub.unsubscribe()
+		}
+	}, [])
+
+	return (
+		<div className="App">
+			<div className="flex justify-center mt-4">
+				<Grid selectedColor={selectedColor} items={items} />
+			</div>
+			<Footer
+				setSelectedColor={setSelectedColor}
+				selectedColor={selectedColor}
+			/>
+		</div>
+	)
+}
+
+const Footer = ({
+	setSelectedColor,
+	selectedColor,
+}: {
+	setSelectedColor: (color: string) => void
+	selectedColor: string
+}) => {
+	const handleColorClick = (color: string) => {
+		setSelectedColor(color)
+	}
+
+	return (
+		<div className="fixed bottom-0 left-0 right-0 bg-gray-800 p-4 flex items-center justify-center space-x-4">
+			{colors.map((color) => (
+				<div
+					key={color}
+					className="w-10 h-10 cursor-pointer"
+					style={{ backgroundColor: color }}
+					onClick={() => handleColorClick(color)}
+				></div>
+			))}
+			<div className="ml-4 text-white">
+				Selected Color: {selectedColor || 'None'}
+			</div>
+		</div>
+	)
+}
+
+export default App
